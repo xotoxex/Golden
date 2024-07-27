10aeadae3d214de52b0460dbef67ca4793c85f3a
xotoxex <kotokek24@gmail.com>
2024-07-27 21:18:13 +0500

    added loader

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// script.js
+window.addEventListener("load", () => {
+  const spinner = document.getElementById("loader");
+  const content = document.getElementById("main_container");
+
+  spinner.classList.add("hidden");
+  content.style.display = "block";
+});
