c028f32ec9d8baa587f6ee0d038b8c178d8d71da
xotoxex <kotokek24@gmail.com>
2024-07-28 17:24:16 +0500

    changed loader

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// script.js
-window.addEventListener("load", () => {
-  const spinner = document.getElementById("loader");
-  const content = document.getElementById("main_container");
+// window.addEventListener("DOMContentLoaded", () => {
+//   const spinner = document.getElementById("loader");
+//   const content = document.getElementById("main_container");
 
-  spinner.classList.add("hidden");
-  content.style.display = "block";
-});
+//   spinner.classList.add("hidden");
+//   content.style.display = "block";
+// });
