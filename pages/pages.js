a844447e6bffebbc51875e0866e497740fbc30d0
xotoxex <kotokek24@gmail.com>
2024-07-28 18:01:05 +0500

    pages daptation

--- pages/pages.js
+++ pages/pages.js
@@ -4,15 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const checkSlideIn = () => {
     const elementTop = element.getBoundingClientRect().top;
+    const elementTop2 = element2.getBoundingClientRect().top;
     const elementVisible = window.innerHeight / 1.2;
 
     if (elementTop < elementVisible) {
       element.classList.add("info-active");
+
+      window.removeEventListener("scroll", checkSlideIn);
+    }
+
+    if (elementTop2 < elementVisible) {
       element2.classList.add("main_hero-active");
+
       window.removeEventListener("scroll", checkSlideIn); // Optional: stop listening after the first animation
     }
   };
 
+  const checkOpacityIn = () => {
+    const elementTop2 = element2.getBoundingClientRect().top;
+    const elementVisible = window.innerHeight / 1.2;
+
+    if (elementTop2 < elementVisible) {
+      element2.classList.add("main_hero-active");
+
+      window.removeEventListener("scroll", checkOpacityIn); // Optional: stop listening after the first animation
+    }
+  };
+
   window.addEventListener("scroll", checkSlideIn);
+  window.addEventListener("scroll", checkOpacityIn);
   checkSlideIn();
 });
